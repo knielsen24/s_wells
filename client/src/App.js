094156ea8bb00f78f264c2ab891b8c9d7a4f6afd
Kevin Nielsen <knielsen24@gmail.com>
2022-10-20 12:21:35 -0700

    set up

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src="" className="App-logo" alt="logo" />
+                
                 <p>
                     300 South Wells
                 </p>
