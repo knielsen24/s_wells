359a3d0e1f0968ed82575f93bb84995b87169976
Kevin Nielsen <knielsen24@gmail.com>
2022-10-28 15:47:03 -0700

    none

--- client/src/assets/WebsiteLinks.js
+++ client/src/assets/WebsiteLinks.js
@@ -1,3 +1,4 @@
 const handCraftURL = "https://www.handcraftcoffeecompany.com/"
 const dorindasURL = "https://dorindaschocolates.com/"
 const theMillBarreURL = "https://themillpilates.com/"
+const atelierURL = "https://www.atelierbeautybar.com/"
