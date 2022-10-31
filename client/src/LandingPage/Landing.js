916be65d5293d8c1f178bb146f9804b5a070ebe4
Kevin Nielsen <knielsen24@gmail.com>
2022-10-31 15:15:28 -0700

    minor

--- client/src/LandingPage/Landing.js
+++ client/src/LandingPage/Landing.js
@@ -1,8 +1,13 @@
+import propertyImage from "../assets/1C-website-cover-v2.jpg"
 import BrandsContainer from "./BrandsContainer";
 
 function Landing() {
     return (
         <div className="text-center h-75">
+            <div className="">
+                <img className="rounded"
+                src={propertyImage}></img>
+            </div>
             <BrandsContainer />
         </div>
     );
