36628a56189d65cf0c0f53b3896e37566679ddd7
Kevin Nielsen <knielsen24@gmail.com>
2022-11-04 11:38:41 -0700

    footer minor change

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -2,22 +2,21 @@ import southWellsLogo from "../assets/Logos/3SW-logo.png";
 
 function Footer() {
     return (
-        <footer className="mt-auto border-top border-1 footer-bg text-white bg-gradient ">
-            <div className="container text-center">
-                <div className="row align-items-center justify-content-between ">
-                    <div className="col ">
-                        <a className="" href="/">300 South Wells
-                            {/* <img
+        <footer className="mt-auto container border-top border-1 footer-bg text-dark bg-gradient ">
+            <div className="row align-items-center justify-content-between ">
+                <div className="col ">
+                    300 South Wells
+                    <a className="" href="/">
+                        {/* <img
                                 src={southWellsLogo}
                                 alt="3sw-logo"
                                 width="100"
                                 id="logo-name"
                             /> */}
-                        </a>
-                    </div>
-                    <div className="col">{/* Login */}</div>
-                    <div className="col">{/* Contact Us*/}</div>
+                    </a>
                 </div>
+                <div className="col">{/* Login */}</div>
+                <div className="col">{/* Contact Us*/}</div>
             </div>
         </footer>
     );
