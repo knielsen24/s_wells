7ee38854ae7ef59336899096ae185a6f56a2b0f2
Kevin Nielsen <knielsen24@gmail.com>
2023-02-07 11:44:25 -0800

    footer layout adjustments

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -1,11 +1,12 @@
 import southWellsLogo from "../assets/Logos/3SW-logo-rectangle.png";
+import knLogo from "../assets/Logos/Logo KN purple.png";
 
 function Footer() {
     return (
         <div className="footer d-flex mt-auto align-items-center border-top border-1 bg-light text-dark bg-gradient ">
-            <div className="container text-center py-2">
-                <div className="row justify-content-between">
-                    <div className="col align-self-center mx-3 my-3">
+            <div className="container py-2">
+                <div className="row gap-4 text-center justify-content-between">
+                    <div className="col mx-3 my-auto">
                         <img
                             src={southWellsLogo}
                             alt="3sw-logo"
@@ -15,7 +16,19 @@ function Footer() {
                         {/* <a className="" href="/"></a> */}
                     </div>
                     {/* <div className="col mx-3 my-3">Login</div> */}
-                    <div className="col mx-3 my-3">Designed & built by Kevin Nielsen</div>
+                    <div className="col d-flex mx-2 my-auto ">
+                        <p className="text-center align-self-center my-auto px-2">
+                            Designed & built by{" "}
+
+                        </p>
+                        <img
+                                className="rounded-2 p-1 knLogo"
+                                src={knLogo}
+                                alt="Kevin Nielsen Logo"
+                                width="35"
+                                // id="logo-name"
+                            ></img>
+                    </div>
                 </div>
             </div>
         </div>
