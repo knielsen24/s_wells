e49f27bf4af068d6c0011b200a70e6a221899548
Kevin Nielsen <knielsen24@gmail.com>
2023-02-07 12:05:08 -0800

    footer margins fixed

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -4,9 +4,9 @@ import knLogo from "../assets/Logos/Logo KN purple.png";
 function Footer() {
     return (
         <div className="footer d-flex mt-auto align-items-center border-top border-1 bg-light text-dark bg-gradient ">
-            <div className="container py-2">
-                <div className="row gap-4 text-center justify-content-between">
-                    <div className="col mx-3 my-auto">
+            <div className="container text-center py-2">
+                <div className="row gap-4 align-items-center justify-content-between">
+                    <div className="col pb-1 mx-3 my-auto">
                         <img
                             src={southWellsLogo}
                             alt="3sw-logo"
@@ -16,18 +16,23 @@ function Footer() {
                         {/* <a className="" href="/"></a> */}
                     </div>
                     {/* <div className="col mx-3 my-3">Login</div> */}
-                    <div className="col d-flex mx-2 my-auto ">
-                        <p className="text-center align-self-center my-auto px-2">
-                            Designed & built by{" "}
-
-                        </p>
-                        <img
-                                className="rounded-2 p-1 knLogo"
-                                src={knLogo}
-                                alt="Kevin Nielsen Logo"
-                                width="35"
-                                // id="logo-name"
-                            ></img>
+                    <div className="col mx-3 my-auto ">
+                        <div className="row justify-content-center">
+                            <div className="col align-self-center text-end p-1">
+                                {" "}
+                                Designed & built by{" "}
+                            </div>
+                            <div className="col align-self-center text-start p-1">
+                                {" "}
+                                <img
+                                    className="rounded-2 p-1 knLogo"
+                                    src={knLogo}
+                                    alt="Kevin Nielsen Logo"
+                                    width="35"
+                                    // id="logo-name"
+                                ></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
